9065891d9643b72262a46d72174b1f806c76fc6b
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-10-29 06:32:46 +0300

    Sandbox - Test remove assign computer feature

--- project/src/Main.java
+++ project/src/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
         Student student1 = new Student("2", "student1", "student@ozu.edu.tr", "student123");
         Technician technician = new Technician("3", "technician1", "technician@ozu.edu.tr", "technician123");
-        Computer windowsComputer = ComputerFactory.createComputer("Windows", "Dell XPS", "16GB", "512GB SSD");
-        technician.assignComputer(student1, windowsComputer);
+        Computer windowsComputer_1 = ComputerFactory.createComputer("Windows", "Dell XPS", "16GB", "512GB SSD");
+        Computer windowsComputer_2 = ComputerFactory.createComputer("Mac", "Mac Air", "8GB", "256GB SSD");
+        technician.assignComputer(student1, windowsComputer_1);
+        technician.assignComputer(student1, windowsComputer_2);
+        student1.getAssignedComputers();
+        System.out.println("Remove Windows : ");
+        technician.removeAssignedComputer(student1, windowsComputer_1);
         student1.getAssignedComputers();
         student1.sendJobRequest();
     }
