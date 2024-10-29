b0774606c9ad288f7883149cf5694b76d0d58fa6
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-10-29 06:22:29 +0300

    Sandbox - Test assign computer feature

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Admin admin = new Admin("1", "adimn", "admin.admin@ozu.edu.tr", "admin123");
-        admin.manageUsers();
+
         Student student1 = new Student("2", "student1", "student@ozu.edu.tr", "student123");
+        Technician technician = new Technician("3", "technician1", "technician@ozu.edu.tr", "technician123");
+        Computer windowsComputer = ComputerFactory.createComputer("Windows", "Dell XPS", "16GB", "512GB SSD");
+        technician.assignComputer(student1, windowsComputer);
+        student1.getAssignedComputers();
         student1.sendJobRequest();
     }
 }
