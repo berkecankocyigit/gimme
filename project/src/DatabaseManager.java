bde84ba94e8c87be574695d305e1062ab4c8d6ce
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-11-08 00:35:57 +0300

    Sandbox - Add database manager

---
+++ project/src/DatabaseManager.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/database_deneme"; // Veritabanı URL'si
+    private static final String DB_USER = "postgres";  // PostgreSQL kullanıcısı
+    private static final String DB_PASSWORD = "password";  // PostgreSQL şifresi
+
+    private static final DatabaseManager ourInstance = new DatabaseManager();
+
+    private DatabaseManager() {
+
+        System.out.println("DatabaseManager is initialized");
+    }
+
+    public static DatabaseManager getInstance() {
+        return ourInstance;
+    }
+
+    public Connection connect() {
+        Connection con = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            if (con != null) {
+                System.out.println("Database connection established");
+            }else{
+                System.out.println("Database connection failed");
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return con;
+
+    }
+}
