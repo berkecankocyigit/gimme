147b45934e5d8f10e78b0c665dd7669739a8328b
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-11-08 22:47:56 +0300

    Sandbox - Edit Main for Testing

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,5 +1,6 @@
 import Computers.Computer;
 import Computers.ComputerFactory;
+import Computers.ComputerType;
 import Users.Student;
 import Users.Technician;
 
@@ -9,12 +10,13 @@ public class Main {
     public static void main(String[] args) {
         DatabaseManager databaseConnector = DatabaseManager.getInstance();
         Connection con = databaseConnector.connect();
+        ComputerFactory computerFactory = new ComputerFactory();
 
         Student student1 = new Student(1, "student1", "student1@ozu.edu.tr", "s1_password");
         Student student2 = new Student(2, "student2", "student2@ozu.edu.tr", "s2_password");
 
-        Computer windowsComputer = ComputerFactory.createComputer(1, "Windows", "Dell XPS", "16GB", "512GB SSD");
-        Computer macComputer = ComputerFactory.createComputer(2,"Mac", "Mac Air", "8GB", "256GB SSD");
+        Computer windowsComputer = computerFactory.createComputer(ComputerType.WindowsComputer, 1, "Dell XPS", "16GB", "512GB SSD");
+        Computer macComputer = computerFactory.createComputer(ComputerType.MacComputer, 2, "Mac Air", "8GB", "256GB SSD");
 
         Technician technician = new Technician(3, "technician1", "technician1@ozu.edu.tr", "t1_password");
 
@@ -28,8 +30,8 @@ public class Main {
         technician.assignComputer(con, student1, windowsComputer);
         technician.assignComputer(con, student1, macComputer);
 
-        student1.showUserComputers(con);
-        student2.showUserComputers(con);
+        student1.printAssignedComputers();
+        student2.printAssignedComputers();
 
         System.out.println("***************************************");
         technician.removeUser(con, student1);
