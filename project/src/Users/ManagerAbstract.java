2b0608c3b23d780b6f57ffef9d0b3b977c409721
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-11-18 13:44:37 +0300

    Sandbox - Add ManagetAbastract class for Admin and Technician

--- project/src/Users/ManagerAbstract.java
+++ project/src/Users/ManagerAbstract.java
@@ -1,9 +1,10 @@
 package Users;
+import Commanders.*;
 import Computers.Computer;
 import Computers.ComputerIterator;
 import Computers.ComputerType;
 import Computers.WindowsComputerFactory;
-import Database.DatabaseManager;
+import Database.DatabaseConnector;
 
 import java.sql.Connection;
 import java.sql.Statement;
@@ -12,153 +13,55 @@ import java.sql.ResultSet;
 import java.util.*;
 
 public abstract class ManagerAbstract extends User {
-    DatabaseManager databaseConnector = DatabaseManager.getInstance();
+    DatabaseConnector databaseConnector = DatabaseConnector.getInstance();
     Connection conn = databaseConnector.connect();
+
+    SimpleRemoteControl remoteControl = new SimpleRemoteControl();
 
     public ManagerAbstract(int id, String name, String email, String password) {
         super(id, name, email, password);
     }
 
     public void addUser( User user){
-        Statement statement;
-        try {
-            String query=String.format("INSERT INTO users(id, name, email, password, status) VALUES ('%s','%s','%s','%s','%s');",
-                    user.getId(),user.getName(),user.getEmail(),user.getPassword(),user.getStatus()
-            );
-
-            statement=conn.createStatement();
-            statement.executeUpdate(query);
-            System.out.println("Student with ID " + user.getId() + " added");
-        }catch (Exception e){
-            System.out.println(e);
-        }
+        AddUserCommand command = new AddUserCommand(user);
+        remoteControl.setSlot(command);
+        remoteControl.doIt();
     }
 
     public void addComputer( Computer computer){
-        Statement statement;
-        try {
-            String query=String.format("INSERT INTO computers(id, model, ram, storage) VALUES ('%s','%s','%s','%s');",
-                    computer.getId(), computer.getModel(), computer.getRam(), computer.getStorage()
-            );
+        Command command = new AddComputerCommand(computer);
+        remoteControl.setSlot(command);
+        remoteControl.doIt();
 
-            statement=conn.createStatement();
-            statement.executeUpdate(query);
-            System.out.println("Computer with ID " + computer.getId() + " added");
-        }catch (Exception e){
-            System.out.println(e);
-        }
     }
 
     public void assignComputer( Student student, Computer computer) {
-        Statement statement;
-        try {
-            String query = String.format(
-                    "INSERT INTO user_computers (user_id, computer_id) VALUES ('%s', '%s');",
-                    student.getId(), computer.getId()
-            );
-
-            statement = conn.createStatement();
-            statement.executeUpdate(query);
-
-            student.addAssignedComputer(computer);
-            computer.addAssignedStudent(student);
-
-            System.out.println("Relation between Student ID " + student.getId() + " and Computer ID " + computer.getId() + " added");
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        AssignComputerCommand command = new AssignComputerCommand(student, computer);
+        remoteControl.setSlot(command);
+        remoteControl.doIt();
     }
 
     public void removeUser( User user) {
-        Statement statement;
-        try {
-            String query = String.format(
-                    "DELETE FROM users WHERE id = %d;",
-                    user.getId()
-            );
-
-            statement = conn.createStatement();
-            statement.executeUpdate(query);
-
-            System.out.println("Student with ID " + user.getId() + " removed");
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        RemoveUserCommand command = new RemoveUserCommand(user);
+        remoteControl.setSlot(command);
+        remoteControl.doIt();
     }
 
     public void removeComputer( Computer computer) {
-        Statement statement;
-        try {
-            String query = String.format(
-                    "DELETE FROM computers WHERE id = %d;",
-                    computer.getId()
-            );
-
-            statement = conn.createStatement();
-            statement.executeUpdate(query);
-
-            System.out.println("Computer with ID " + computer.getId() + " removed");
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        RemoveComputerCommand command = new RemoveComputerCommand(computer);
+        remoteControl.setSlot(command);
+        remoteControl.doIt();
     }
 
     public void removeAssignedComputer( Student student, Computer computer) {
-        Statement statement;
-        try {
-            String query = String.format(
-                    "DELETE FROM user_computers WHERE user_id = %d AND computer_id = %d;",
-                    student.getId(), computer.getId()
-            );
-
-            statement = conn.createStatement();
-            statement.executeUpdate(query);
-
-            student.removeAssignedComputer(computer);
-            computer.removeAssignedStudent(student);
-
-            System.out.println("Relation between Student ID " + student.getId() + " and Computer ID " + computer.getId() + " removed");
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        RemoveAssignedComputerCommand command = new RemoveAssignedComputerCommand(student, computer);
+        remoteControl.setSlot(command);
+        remoteControl.doIt();
     }
 
     public Iterator getComputers() {
-        Set<Computer> computers = new HashSet<>();
-        Statement statement;
-        ResultSet resultSet;
-
-        WindowsComputerFactory windowsComputerFactory = new WindowsComputerFactory();
-
-        try {
-            String query = "SELECT id, model, ram, storage FROM computers";
-            statement = conn.createStatement();
-
-            resultSet = statement.executeQuery(query);
-
-            while (resultSet.next()) {
-                int id = Integer.parseInt(resultSet.getString("id"));
-                String model = resultSet.getString("model");
-                String ram = resultSet.getString("ram");
-                String storage = resultSet.getString("storage");
-
-                // Computer nesnesini oluştur ve listeye ekle
-                Computer computer = windowsComputerFactory.createComputer(id, model, ram, storage);
-                computers.add(computer);
-            }
-
-            // Kaynakları serbest bırak
-            resultSet.close();
-            statement.close();
-        } catch (Exception e) {
-            System.out.println("Error retrieving computers: " + e);
-        }
-
-        // Bilgisayar listesini döndür
-        return new ComputerIterator(computers);
+        GetComputersCommand command = new GetComputersCommand();
+        remoteControl.setIterator(command);
+        return remoteControl.get_Computer();
     }
 }
